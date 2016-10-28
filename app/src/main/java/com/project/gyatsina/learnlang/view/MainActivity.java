e2b8a216879faea10d59c95f235eef7b1ea7a6cf
gyatsina <galina_kharkov@inbox.ru>
2016-10-28 22:00:47 +0200

    Reworked layout elements retrieving to findbyid annotation; extracted view initializations to separate method

--- app/src/main/java/com/project/gyatsina/learnlang/view/MainActivity.java
+++ app/src/main/java/com/project/gyatsina/learnlang/view/MainActivity.java
@@ -20,26 +20,32 @@ import com.project.gyatsina.learnlang.view.adapter.CourseAdapter;
 
 import org.androidannotations.annotations.ViewById;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity{
 //        implements NavigationView.OnNavigationItemSelectedListener {
 
+    @ViewById(R.id.fab)
+    FloatingActionButton fab;
+
+    @ViewById(R.id.toolbar)
+    Toolbar toolbar;
+
+    @ViewById(R.id.drawer_layout)
+    DrawerLayout drawer;
+
     @ViewById(R.id.post_list)
-    RecyclerView postList;
+    RecyclerView courseList;
 
     private CourseAdapter courseAdapter;
-    private LinearLayoutManager postListLayoutManager;
+    private LinearLayoutManager courseListLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ((LearnLangApplication) getApplication()).component().inject(this);
+        initViews();
 
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,14 +54,31 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
+    private void initViews() {
+        ((LearnLangApplication) getApplication()).component().inject(this);
+
+        setContentView(R.layout.activity_main);
+        setSupportActionBar(toolbar);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        courseListLayoutManager = new LinearLayoutManager(this);
+        courseList.setLayoutManager(courseListLayoutManager);
+
+        courseAdapter = new CourseAdapter();
+        courseList.setAdapter(courseAdapter);
+
 //        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 //        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void initBindings(){
+
     }
 
     @Override
