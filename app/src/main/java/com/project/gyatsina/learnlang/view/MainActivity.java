ec245130d29494c580c3a4516fd5b6462ffb6582
gyatsina <galina_kharkov@inbox.ru>
2016-11-15 18:37:22 +0100

    Removed NavigationView. Fixed DrawerLayout to use RecycleView correctly (layout with drawer is SECOND and need to have id and gravity)

--- app/src/main/java/com/project/gyatsina/learnlang/view/MainActivity.java
+++ app/src/main/java/com/project/gyatsina/learnlang/view/MainActivity.java
@@ -1,13 +1,11 @@
 package com.project.gyatsina.learnlang.view;
 
-import android.app.ActionBar;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -23,12 +21,10 @@ import com.project.gyatsina.learnlang.view.adapter.CourseAdapter;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends AppCompatActivity {
-//        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity{
 
     @ViewById(R.id.fab)
     FloatingActionButton fab;
@@ -45,23 +41,21 @@ public class MainActivity extends AppCompatActivity {
     private CourseAdapter courseAdapter;
     private LinearLayoutManager courseListLayoutManager;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        System.out.println("vth673  onCreate");
         ((LearnLangApplication) getApplication()).component().inject(this);
-//        setContentView(R.layout.activity_main);
     }
 
     @AfterViews
     void initViews() {
         System.out.println("vth673   initViews");
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
-//        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,mDrawerLayout
-//                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close)
-//        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
             public void onDrawerClosed(View view) {
@@ -94,8 +88,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void initBindings() {
@@ -133,28 +125,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-//    @SuppressWarnings("StatementWithEmptyBody")
-//    @Override
-//    public boolean onNavigationItemSelected(MenuItem item) {
-    // Handle navigation view item clicks here.
-//        int id = item.getItemId();
-//
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        drawer.closeDrawer(GravityCompat.START);
-//        return true;
-//    }
 }
